84b5a0ddb61856f7eb75097c1de2adb24d3d0e4c
khinelwinoo <89257389+khinelwinoo@users.noreply.github.com>
2022-12-22 12:18:42 +0630

    Initial Commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from wtforms import StringField, SubmitField
+from wtforms.validators import Email, DataRequired, Length
+import smtplib
+
+app = Flask(__name__)
+app.secret_key = "bingchulovesgyugyuandmandu"
+bootstrap = Bootstrap(app)
+
+MY_EMAIL = "gyumandu@gmail.com"
+MY_PASSWORD = "lzkmljvevabvwewd"
+
+
+class ContactFrom(FlaskForm):
+    name = StringField("Name",
+                       validators=[DataRequired(), Length(max=255)])
+    email = StringField("Email",
+                        validators=[DataRequired(), Email(message="Invalid Email Address"), Length(max=255)])
+    message = StringField("Message",
+                          validators=[DataRequired(), Length(max=500)])
+    submit = SubmitField("Send Message")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    form = ContactFrom()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
+        contact_message = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+        with smtplib.SMTP("smtp.gmail.com") as smtp:
+            smtp.starttls()
+            smtp.login(user=MY_EMAIL, password=MY_PASSWORD)
+            smtp.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=MY_EMAIL,
+                msg=f"Subject: New Message Alert !!\n\n{contact_message}".encode("utf-8")
+            )
+        return render_template("thankyou.html", is_thankyou_page=True)
+    return render_template("contact.html", form=form, is_contact_page=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
